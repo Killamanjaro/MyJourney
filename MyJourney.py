169bfa36dc46ef76e3f5c8938b7358452c0dbe5b
Killamanjaro <61355379+Killamanjaro@users.noreply.github.com>
2023-05-06 13:09:27 +0100

    Add files via upload

---
+++ MyJourney.py
@@ -0,0 +1,60 @@
+#This is a chroncal of MyJourney after learning about my layoff.
+#Day 1
+#Learning about Python to understand the journey my own team members went through in our delivery of a secure software supply chain.
+print("**Day 1**")
+print("Hello,World!")
+print("wish me luck!I am looking for a new job!")
+day1 = "Here I go!"
+print(day1)
+
+#Day 2
+print("**Day 2**")
+print("Trying to figure out the next job")
+options = "Cybersecurity?", "DevOps?", "FinOps?", "M&A?", "Architect?"
+a,b,c,d,e = options
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+#I settled on studying AWS architecture Day 2
+
+#Day 3
+print("**Day 3**")
+print("Worried about architects. 190k tech layoffs in the last 4 months!")
+#Hours per day to study each subject
+print("I have 7 hours per day to study up for my next journey. How should I break it down?")
+print("This is how I did.")
+a = 3
+b = "hours Architecture"
+c = 1
+d = "hour FinOps"
+e = 2
+f = "hours Cybersecurity"
+g = 1
+h = "hour Job networking"
+print(a,b)
+print(c,d)
+print(e,f)
+print(g,h)
+
+#Day 4-10
+print("Days 4-10")
+print("Too much context switching. Decided to focus my 7 hours each day to 2 subjects.")
+a = 3
+b = "hours architecture"
+c = 3.5
+d = "hours Cybersecurity"
+e = .5
+f = "little bits of python to catalog my journey."
+print(a,b)
+print(c,d)
+print(e,f)
+print("10 days later... LinkedIn, CV done. Progress made on + knowledge!")
+
+
+
+
+
+
+
